64a4a4341045c9cc0957157bf014c45cf091bfd5
manishbupadhyay <ermanishupadhyay@gmail.com>
2018-09-28 23:49:31 +0530

    input property initial checkin

--- hello-world/src/app/favorite/favorite.component.ts
+++ hello-world/src/app/favorite/favorite.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'favorite',
@@ -6,13 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./favorite.component.css']
 })
 export class FavoriteComponent implements OnInit {
-  isFavorite : boolean;
+  @Input('isFavorite') isSelected : boolean;
+  
+  
   constructor() { }
 
   ngOnInit() {
   }
 
   onClick(){
-    this.isFavorite = !this.isFavorite;
+    this.isSelected = !this.isSelected;
   }
 }
